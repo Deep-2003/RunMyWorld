831157a21449394cc51c770f8f51daff463d07af
Ayush Kumar <2k23.csaiml2311319@gmail.com>
2025-11-05 10:40:54 +0530

    Initial commit - RunMyWorld app based on RunAnywhere base

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+        maven { url = uri("https://maven.runanywhere.ai/releases") }
+
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+        maven { url = uri("https://maven.runanywhere.ai/releases") }
+    }
+}
+
+rootProject.name = "startup_hackathon2.0"
+include(":app")
